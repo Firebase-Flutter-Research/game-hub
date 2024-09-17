25b4732c97288193a728d064b83365c503799e57
Kelvin Gonzalez <kelvin.gonzalez11@upr.edu>
2024-09-16 21:18:54 -0400

    Add Android support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fire_engine/page/home.dart';
 import 'package:flutter_fire_engine/page/in_game.dart';
@@ -14,7 +15,9 @@ void main() async {
           projectId: "game-hub-2-283bb",
           storageBucket: "game-hub-2-283bb.appspot.com",
           messagingSenderId: "304830683808",
-          appId: "1:304830683808:web:ce7b8061ac46d9034587c5"));
+          appId: kIsWeb
+              ? "1:304830683808:web:ce7b8061ac46d9034587c5"
+              : "1:304830683808:android:d27335da1d4850504587c5"));
   runApp(const MyApp());
 }
 
